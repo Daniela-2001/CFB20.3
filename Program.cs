c111e50d8b842d72bc2e9948daa70c5af8b0b8d6
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-26 20:13:27 +0000

    CFB20.3

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace aula20._3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] num = new int[10];
+
+            int i = num.Length - 1;
+            while (i > 0)
+            {
+                num[i] = 0;
+                Console.WriteLine(num[i]);
+                i--;
+            }
+
+            Console.WriteLine("\nFim do loop");
+      
+        }
+    }
+}
